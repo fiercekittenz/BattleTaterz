cc9932586035850fada96318d10a7f238d18636f
Georgia Nelson <georgia.e.nelson@gmail.com>
2025-04-12 20:45:26 -0400

    Adding objects representing potential moves and their directions.

---
+++ Objects/Grid/PotentialMoveInfo.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BattleTaterz.Objects.Grid
+{
+    public class PotentialMoveInfo
+   {
+      public Tile TileRef { get; set; } = null;
+
+      public int Row { get; set; } = 0;
+
+      public int Column { get; set; } = 0;
+
+      public MoveDirection Direction { get; set; } = MoveDirection.NONE;
+
+      public PotentialMoveInfo(Tile tileRef, int row, int column, MoveDirection direction)
+      {
+         TileRef = tileRef;
+         Row = row;
+         Column = column;
+         Direction = direction;
+      }
+
+      private PotentialMoveInfo() { }
+   }
+}
